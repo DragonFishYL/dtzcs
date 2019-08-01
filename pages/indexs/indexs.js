46878a8c91e3e7b5c6fb4d61b1f1feeb4f4f376a
DragonFishYL <1435033280@qq.com>
2019-08-01 18:02:06 +0800

    banner好了刚开始

---
+++ pages/indexs/indexs.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+	data: {
+        bannerImages: [ {
+            img: "/public/image/dtzc1564650832.jpg"
+        }, {
+            img: "/public/image/dtzc1564650870.jpg"
+        }, {
+            img: "/public/image/dtzc1564650748.jpg"
+        } , {
+            img: "/public/image/dtzc1564650774.jpg"
+        }, {
+            img: "/public/image/dtzc1564650805.jpg"
+        }],
+	},
+})
